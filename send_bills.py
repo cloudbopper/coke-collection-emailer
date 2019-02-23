e48a2d7a3073332ae976a47a2c7dcb253ee659ca
Akshay Sood <sood.iitd@gmail.com>
2019-02-22 20:54:38 -0600

    Update messages and examples

--- send_bills.py
+++ send_bills.py
@@ -1,10 +1,17 @@
 """Script to send emails for collecting coke money"""
 
 import argparse
+import csv
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from getpass import getpass
 import smtplib
+
+# csv fields
+NAME = "Name"
+EMAIL = "Email"
+BALANCE = "Current balance"
+UNKNOWN = "???"
 
 # czar info
 CZAR_USERNAME = "sacm.coke.czar"
@@ -16,7 +23,8 @@ CZAR_MAILROOM = "MSC 4730"
 # email body
 SUBJECT = "MSC Coke Balance"
 CLOSE_ACCOUNT = "If you would like to close your account (and stop receiving these emails), reply and let me know."
-PAYMENT_DETAILS = ("Payments can be brought to my office ({0}). Alternatively, checks can be left in my mailbox "
+PAYMENT_DETAILS = ("Cash payments can be brought to my office ({0}). Please be sure to indicate your name in case "
+                   "you're not handing over the payment personally. Alternatively, checks can be left in my mailbox "
                    "in {1}. Checks should be written to SACM. I cannot provide change for cash payments, "
                    "but I can carry over any excess payment as a system credit.".format(CZAR_OFFICE, CZAR_MAILROOM))
 THANKS = "Thank you, and please let me know if you believe I've made any error.\n\n- {0}, MSC Coke Czar".format(CZAR_NAME)
@@ -51,8 +59,11 @@ def main():
     server = setup()
 
     with open(args.balance_filename, "r") as balance_file:
-        for line in balance_file:
-            name, destination_email, amount = line.rstrip().split(",")
+        reader = csv.DictReader(balance_file)
+        for row in reader:
+            name, destination_email, amount = (row[key] for key in [NAME, EMAIL, BALANCE])
+            if not destination_email or not name or name == "???":
+                continue
             amount = float(amount.replace("$", ""))
             body = "Hi {0},\n\n".format(name)
             if amount < 0.00:
